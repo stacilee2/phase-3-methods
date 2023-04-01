880d553f578f6c3b24680fc22ed13ee380d4d660
Staci Rosales <stacileep2@gmail.com>
2023-04-01 11:02:09 -0500

    commit tree 10c97d4d0e0650de7a14069533449840a8f72e6e

--- methods.rb
+++ methods.rb
@@ -1,1 +1,35 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+end
+
+greet "Jimmy"
+
+def greet_with_default name="programmer"
+    puts "Hello, #{name}!"
+end
+
+greet_with_default "Naureen"
+
+def add num1, num2
+    num1 + num2
+end
+
+add 2,3
+
+def halve num
+    if num.class != Integer
+        return nil
+    end
+    num / 2
+    end
+
+halve 6
+halve "hello"
+
+
